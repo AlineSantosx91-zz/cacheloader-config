731918003247e20c3b2024899abc092a41b6e3b8
Aline Santos <aline.santos@concrete.com.br>
2017-12-14 19:42:42 -0200

    adiciona logica de iterar pelo json

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,11 @@
-import { Router} from '@angular/router';
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Http } from '@angular/http';
 
 import { FileUploader } from 'ng2-file-upload';
 import { ModalDialogComponent } from './../common/modal-dialog/modal-dialog.component';
 
-declare var jQuery:any;
+declare var jQuery: any;
 
 @Component({
   selector: 'app-menu',
@@ -25,12 +25,6 @@ export class MenuComponent implements OnInit {
   mensagens: string[]
   error: boolean;
   modalDialogComponent: ModalDialogComponent;
-  
-
-  @ViewChild('modal') myModal: ElementRef;
-
-  // @ViewChild('modal', {read: ElementRef}) private myModal: ElementRef;
-  
 
   constructor(private router: Router) {
     this.appCombobox = false;
@@ -70,10 +64,9 @@ export class MenuComponent implements OnInit {
   }
 
   setAplicativo(aplicativo: string) {
-    debugger;
     this.aplicativo = aplicativo;
     this.appRadioButton = true;
-    
+
   }
 
   cadastrarMenu() {
@@ -87,13 +80,69 @@ export class MenuComponent implements OnInit {
       return;
     }
 
-    // this.router.navigate(['/menu-cadastro'])
+    this.varrerMenu();
+  }
 
-    //open modal using jQuery
-    console.log(this.myModal);
-    jQuery(this.myModal.nativeElement).modal('show'); 
+  varrerMenu() {
+    for (var i = 0; i < 100; i++) {
+      var key = Object.keys(this.fileMenu)[i];
+      if (key !== undefined) {
+
+        if (key === this.aplicativo && key !== "mobileitaucard.home-mobile-cartoes") {
+          this.varrerAplicativo(this.fileMenu[key]);
+          break;
+        }
+        if (key === this.aplicativo && key === "mobileitaucard.home-mobile-cartoes") {
+          this.varrerAplicativoCartoes(this.fileMenu[key]);
+          break;
+        }
+      }
+    }
+  }
+
+  varrerAplicativo(aplicativoMenu: string) {
+    for (var i = 0; i < 100; i++) {
+      var key = Object.keys(aplicativoMenu)[i];
+      if (key !== undefined) {
+        if (key === "filhos") {
+          let filhos = new Array;
+          filhos = aplicativoMenu[key];
+          this.obterNumerosDeOrdem(filhos);
+          break;
+        }
+      }
+    }
+  }
+
+  varrerAplicativoCartoes(aplicativoMenu: string) {
+
+  }
+
+  obterNumerosDeOrdem(filhos: Array<String>) {
+    debugger;
+    let ordens = new Array;
+
+    //Todo criar um model de Filho
+  
+    for (var i = 0; i < 100; i++) {
+      var filho = filhos[i];
+      var key = Object.keys(filho)[i];
+      if (key !== undefined) {
+        debugger;
+        if (key === "ordem") {
+          ordens.push(filhos[key]);
+        }
+      }
+    }
+
+    this.retornaProximoNumeroMaior(ordens);
     
-    console.log(this.aplicativo);
-    console.log(this.opcao);
+  }
+
+  retornaProximoNumeroMaior(ordens: Array<Number>) {
+    debugger;
+
+    console.log(ordens);
+
   }
 }
